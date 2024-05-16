6cd704a957c7f09cde548bc14f98c522fce3da3e
wangzhongkai1 <49552212+wangzhongkai1@users.noreply.github.com>
2024-05-16 22:01:55 +0800

    Create 01_filter_illumina.sh

---
+++ 01_filter_illumina.sh
@@ -0,0 +1,1 @@
+perl /public/home/liyongxin/wangzhongkai/script/filter_v1.5/get_clean_shell.wzk.pl -q 33 -t 4 -o 01_filter/clean -i 01_filter/all_illumina.list  -a 5 -b 5 -c 5 -d 5 -dup 1 -queue high2
